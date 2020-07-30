2341c16a8ef34634c3421d593eddd5d4b6e9d89b
ss <ss@example.com>
2020-07-31 02:43:51 +0800

    新建项目testGitToGithub

---
+++ src/com/fs/xixi.java
@@ -0,0 +1,15 @@
+package com.fs;
+
+/**
+ * @ClassName xixi
+ * @Description TODO
+ * @Author ss
+ * @Date 2020/7/31 2:40
+ * @Version 1.0
+ */
+public class xixi {
+
+    public static void main(String[] args) {
+        System.out.println("xixi");
+    }
+}
